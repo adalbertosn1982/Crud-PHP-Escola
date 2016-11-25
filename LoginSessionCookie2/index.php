14d42b16f8758fd5fa8de0c420fddb40660ceed5
adalbertosn1982 <adalbertosn1982@gmail.com>
2016-11-25 16:18:41 -0200

    projeto inicial

---
+++ LoginSessionCookie2/index.php
@@ -0,0 +1,13 @@
+<?PHP
+session_start();
+if(isset($_COOKIE) && count($_COOKIE) >0 ){
+	print_r($_COOKIE);
+}
+if(isset($_SESSION) && count($_SESSION) >0){
+	print_r($_SESSION);
+	
+}
+
+
+include_once( dirname(__FILE__).'\login_frm.php');
+?>
---
+++ LoginSessionCookie2/login_frm.php
@@ -0,0 +1,8 @@
+
+
+<form action="" method="POST">
+	<p>Nombre:<input type="text" placeholder="Tu Nombre!" name="nombre" required/></p><br />
+	<p>Senha:<input type="password" name="pw" required/></p><br />
+	<input type="checkbox" name="guardar_clave" value="1"> Memorizar el usuario en este ordenador<br />
+	<input type="submit" value="Enviar" />
+</form>
